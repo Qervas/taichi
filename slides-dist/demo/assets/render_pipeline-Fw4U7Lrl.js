e11f29ad3c7c2ceae6973c64021479aa9b5a3e76
Qervas <djmax96945147@outlook.com>
2026-02-28 09:20:44 +0100

    Fix slides 404: rebuild with --base ./ for relative asset paths; smoke date → Feb 24

---
+++ slides-dist/demo/assets/render_pipeline-Fw4U7Lrl.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../render_pipeline.png",import.meta.url).href;export{e as _};
---
+++ slides-dist/phase2/assets/slidev/default-QCPF7EtB.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-BOLmPnWG.js";import{_ as r}from"../index-D3SD0EfW.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
